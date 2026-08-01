83d25ccc948d5dd61fc2309cd9a154b6fe0c4c6d
erikaaaaa7 <julianne_erika_deleon@dlsu.edu.ph>
2026-08-01 17:37:33 +0800

    Update menu.c

--- menu.c
+++ menu.c
@@ -1,42 +1,59 @@
 #include <stdio.h>
 #include <string.h>
 
-int main()
+void separator1()
 {
-	int opt = 0;
-	
+	printf("*************************************** \n");
+}
+
+void menuTab()
+{
 	printf("RAT SIMULATOR \n");
 	printf("\n");
 	printf("[1] Start Simulator \n");
 	printf("[2] Load File \n");
 	printf("[3] Quit \n");
 	printf("\n");
+}
+
+int main()
+{
+	int opt = 0;
 	
-	scanf(">> %d", &opt);
+	separator1();
+	menuTab();
 	
-	if(opt != 1 || opt != 2 || opt != 3)
+	separator1();
+	
+	printf(">> ");
+	scanf("%d", &opt);
+	
+	while(opt != 1 && opt != 2 && opt != 3)
 	{
-		printf("Invalid input! Press Enter to try again...");
+		printf("Invalid input! Press Enter to try again... \n");
 		while(getchar() != '\n');
         getchar();
-        option = 0; 
+        opt = 0; 
+        
+        separator1();
+        printf(">> ");
+		scanf("%d", &opt);
 	}
-	else
-	{
+
 		if(opt == 1)
 		{
-			printf("1");
+			printf("test 1");
 		}
 		else if(opt == 2)
 		{
-			printf("2");
+			printf("test 2");
 		}
 		else if(opt == 3)
 		{
-			printf("3");
+			printf("test 3");
 		}
 		
-	}
+	
 	
 	
 }
